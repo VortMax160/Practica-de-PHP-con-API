5d0a5029c15afdfe4ab6fc0a051ae86b2c0f68a5
VortMax160 <maximusale@outlook.com>
2024-08-07 19:26:22 -0300

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <title>Proxima pelicula de Marvel </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
-    >
-</head>
